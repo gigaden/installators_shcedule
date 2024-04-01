4fee9cff7d1a211b30fc90616a2118fef790d517
gigaden <gigaden@ya.ru>
2024-04-01 13:33:53 +0300

    added april to winter gaz tax

--- settings.py
+++ settings.py
@@ -6,7 +6,5 @@ current_month = current_date.month
 current_year = current_date.year
 
 # стоимость 1 км пути в рублях в зависимости от месяца(лето/зима)
-GAZ_TAX = 11.28 if current_month in (11, 12, 1, 2, 3) else 9.73
+GAZ_TAX = 11.28 if current_month in (11, 12, 1, 2, 3) else 10.25
 SCORE_PRICE = 102  # стоимость балла по умолчанию, юзер сможет потом изменить
-
-print(current_month)
